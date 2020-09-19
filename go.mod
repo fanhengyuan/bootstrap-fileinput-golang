c70f6762c7a7a45da04bd6b308d3be961d64e002
fanhengyuan <mrfhy@qq.com>
2020-09-19 19:37:57 +0800

    update .gitignore

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module upload
 
 go 1.15
+
+require github.com/rakyll/statik v0.1.7
--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"log"
 	"net/http"
 	"upload/controllers"
+	_ "upload/statik"
 )
 
 func main() {
 	log.Println("静态服务已启动:8877")
 	
 	// 静态资源
+/*	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fs := http.FileServer(statikFS)
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/", http.FileServer(http.Dir("./")))*/
+
 	fs := http.FileServer(http.Dir("./fileinput"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.Handle("/", http.FileServer(http.Dir("./")))
@@ -17,5 +26,8 @@ func main() {
 	// 上传服务
 	http.HandleFunc("/upload", controllers.UploadFile)
 	
-	http.ListenAndServe(":8877", nil)
+	error := http.ListenAndServe(":8877", nil)
+	if error != nil {
+		log.Println(error.Error())
+	}
 }

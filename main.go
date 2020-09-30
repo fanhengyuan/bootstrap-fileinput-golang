3c50fc6c6a2146b5c331a23b87fbde013bc2bf05
fanhengyuan <mrfhy@qq.com>
2020-09-30 16:34:46 +0800

    style

--- main.go
+++ main.go
@@ -4,8 +4,24 @@ import (
 	"log"
 	"net/http"
 	"upload/controllers"
+	"strings"
+	"encoding/base64"
 	//_ "upload/statik"
 )
+
+func checkAuth(w http.ResponseWriter, r *http.Request) bool {
+	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(s) != 2 { return false }
+
+	b, err := base64.StdEncoding.DecodeString(s[1])
+	if err != nil { return false }
+
+	pair := strings.SplitN(string(b), ":", 2)
+	if len(pair) != 2 { return false }
+
+	return pair[0] == "user" && pair[1] == "pass"
+}
+
 
 func main() {
 	log.Println("静态服务已启动:8877")
@@ -22,7 +38,7 @@ func main() {
 	fs := http.FileServer(http.Dir("./fileinput"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.Handle("/", http.FileServer(http.Dir("./")))
-	
+
 	// 上传服务
 	http.HandleFunc("/upload", controllers.UploadFile)
 	
